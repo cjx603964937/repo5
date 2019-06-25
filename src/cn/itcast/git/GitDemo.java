aae244b4d0e1556d8782f1a986ee2b6fc53a627e
taylor <603964937@qq.com>
2019-06-25 08:34:10 +0800

    设置忽略

---
+++ src/cn/itcast/git/GitDemo.java
@@ -0,0 +1,11 @@
+package cn.itcast.git;
+
+public class GitDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello motherFucker!");
+        //https
+        //忽略.idea
+    }
+}
